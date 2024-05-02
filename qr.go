3d9d9bb353cc6a8d054b9140a23af6055bbade8e
Andrei Buiciuc <andreibuiciuc.business@gmail.com>
2024-05-02 16:58:05 +0300

    change qr encoding entry point

--- qr.go
+++ qr.go
@@ -4,41 +4,37 @@ import "fmt"
 
 type Qr struct{}
 
-func New(*Qr) *Qr {
+func New() *Qr {
 	return &Qr{}
 }
 
-func (qr *Qr) Encode(s string, lvl rune, filename string) (Image[module], error) {
+func (qr *Qr) Encode(s string, lvl rune, filename string) (*matrix[module], error) {
 	versioner := newVersioner()
-	mode, err := versioner.getMode(s)
 
+	mode, err := versioner.getMode(s)
 	if err != nil {
 		return nil, fmt.Errorf("error")
 	}
 
 	version, err := versioner.getVersion(s, mode, lvl)
-
 	if err != nil {
 		return nil, fmt.Errorf("error")
 	}
 
 	encoder := newEncoder()
 	encoded, err := encoder.encode(s, lvl)
-
 	if err != nil {
 		return nil, fmt.Errorf("error")
 	}
 
 	encoded = encoder.augmentEncodedInput(encoded, version, lvl)
-
 	interleaver := newInterleaver()
 	encoded = interleaver.getFinalMessage(encoded, version, lvl)
-
 	moduler := newModuler(version, lvl)
 	matrix, _ := moduler.createModuleMatrix(encoded)
 
-	image := NewImage()
-	image.CreateImage(filename, matrix.GetMatrix())
-
-	return image, nil
+	// image := NewImage()
+	// image.CreateImage(filename, matrix.GetMatrix())
+	// return image, nil
+	return matrix, nil
 }
